638b70266a720772b61b371eadc6d0a7f79a10f0
lixiaolongxl <lxldevtest@163.com>
2021-12-06 21:03:50 +0800

    them custom

--- shader.js
+++ shader.js
@@ -3,13 +3,12 @@ global.THREE = require("three");
 
 // Include any additional ThreeJS examples below
 require("three/examples/js/controls/OrbitControls");
-const Random = require("canvas-sketch-util/random");
+
 const canvasSketch = require("canvas-sketch");
-const glsl = require("glslify");
+// const FBXLoader = require('three/examples/jsm/loaders/FBXLoader') ;
+// import {FBXLoader}  from 'three/examples/jsm/loaders/FBXLoader'
 const settings = {
-    duration:5,
-    dimensions:[1080,1080],
-    fps:60,
+    
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
@@ -21,96 +20,100 @@ const sketch = ({ context }) => {
   const renderer = new THREE.WebGLRenderer({
     canvas: context.canvas
   });
-
+  renderer.shadowMap.enable = true;
+//   var fbxLoader = new FBXLoader()
   // WebGL background color
-  renderer.setClearColor("#fff", 1);
+    renderer.setClearColor("#fff", 1);
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+	renderer.outputEncoding = THREE.sRGBEncoding;
+
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
-  camera.position.set(0, 0, -4);
-  camera.lookAt(new THREE.Vector3());
+  const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 1000 );
+//   const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(160, 40, 10);
+//   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
   const controls = new THREE.OrbitControls(camera, context.canvas);
-
+  
+  controls.minDistance = 20;
+  controls.maxDistance = 500;
+  controls.enablePan = false;
   // Setup your scene
   const scene = new THREE.Scene();
 
-  // Setup a geometry
-  const geometry = new THREE.SphereGeometry(1, 32, 16);
-  const baseGem  = new THREE.IcosahedronGeometry(1, 1);
-  const circleGem = new THREE.CircleGeometry(1,32);
-  const points = baseGem.vertices;
-//   console.log(points);
-  
-  
-//   const geometry = new THREE.TorusGeometry(1,0.5,32, 16);
-  const vertexShader = /* glsl*/ `
-  varying vec2 vUv;
-  varying vec3 vPosition;
-    void main() {
-      vUv = uv;
-      vPosition= position;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
-    }
-  `;
-  const fragmentShader =glsl( /* glsl */`
-  #pragma glslify: noise = require('glsl-noise/simplex/3d');
-  #pragma glslify: aastep = require('glsl-aastep');
-  varying vec2 vUv;
-  uniform vec3 color;
-  uniform float time;
-  varying vec3 vPosition;
-  uniform vec3 points[POINT_COUNT];
-  uniform mat4 modelMatrix;
+  //set your lights
+  const ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
+    scene.add( ambient );
 
-    float sphereRim (vec3 spherePosition) {
-        vec3 normal = normalize(spherePosition.xyz);
-        vec3 worldNormal = normalize(mat3(modelMatrix) * normal.xyz);
-        vec3 worldPosition = (modelMatrix * vec4(spherePosition, 1.0)).xyz;
-        vec3 V = normalize(cameraPosition - worldPosition);
-        float rim = 1.0 - max(dot(V, worldNormal), 0.0);
-        return pow(smoothstep(0.0, 1.0, rim), 0.5);
-    }
+    const spotLight = new THREE.SpotLight( 0xffffff, 1 );
+    spotLight.position.set( 15, 40, 35 );
+    spotLight.angle = Math.PI / 4;
+    spotLight.penumbra = 0.1;
+    spotLight.decay = 2;
+    spotLight.distance = 400;
 
-    void main() {
-        float dist = 1000.0;
-        for (int i = 0; i < POINT_COUNT; i++) {
-          vec3 point = points[i];
-          float curDist = distance(vPosition, point);
-          dist = min(curDist, dist);
-        }
-        float mask = aastep(0.15,dist);
-        mask = 1.0 - mask;
-        
-        vec3 fragColor = mix(color, vec3(1.0), mask);
-        float rim = sphereRim(vPosition);
-        fragColor += rim * 0.25;
-        gl_FragColor = vec4(vec3( fragColor),1.0);
-    }
-  `);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = window.innerWidth;
+    spotLight.shadow.mapSize.height = window.innerHeight;
+    spotLight.shadow.camera.near = 10;
+    spotLight.shadow.camera.far = 200;
+    spotLight.shadow.focus = 2;
+    scene.add( spotLight );
+    
 
-  // Setup a material
-  const material = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    defines:{
-        POINT_COUNT:points.length
-    },
-    extensions:{
-        derivatives:true,
-    },
-    uniforms:{
-    points:{value:points},
-      time:{value:0},
-      color:{value:new THREE.Color('tomato')}
-    },
-    // wireframe: true
-  });
- 
-  // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+	
+
+
+			
+                //
+
+				let material = new THREE.MeshPhongMaterial( { color: 0x808080, dithering: true } );
+
+				let geometry = new THREE.PlaneGeometry( 2000, 2000 );
+
+				let mesh = new THREE.Mesh( geometry, material );
+				mesh.position.set( 0, - 1, 0 );
+				mesh.rotation.x = - Math.PI * 0.5;
+				mesh.receiveShadow = true;
+				scene.add( mesh );
+
+				//
+
+				material = new THREE.MeshPhongMaterial( { color: 0x4080ff, dithering: true } );
+                // var edgesMtl = new THREE.PointsMaterial({
+                //     color: 0xff0000,
+                //     size: 5.0 //点对象像素尺寸
+                //   }); //材质对象
+				// geometry = new THREE.CylinderGeometry( 5, 5, 2, 32, 1, false );
+                geometry = new THREE.BoxGeometry(10, 10, 10); //创建一个立方体几何对象Geometry
+				mesh = new THREE.Mesh( geometry, material );
+				mesh.position.set( 0, 0, 0 );
+				mesh.castShadow = true;
+
+                const edges = new THREE.EdgesGeometry( geometry );
+                let edgesMtl =  new THREE.LineBasicMaterial({color: 0xff0000});
+                
+                
+                const line = new THREE.LineSegments(edges,edgesMtl)
+                
+				scene.add( mesh );    
+                scene.add( line );
+
+
+                var geometrylxl = new THREE.BoxGeometry(10, 10, 10); //创建一个立方体几何对象Geometry
+    // 点渲染模式
+    var materiallxl = new THREE.PointsMaterial({
+      color: 0xff0000,
+      size: 1.0 //点对象像素尺寸
+    }); //材质对象
+    var points = new THREE.Points(geometrylxl, materiallxl); //点模型对象
+    scene.add(points); //点对象添加到场景中
+
+
+
+    
 
   // draw each frame
   return {
@@ -122,10 +125,12 @@ const sketch = ({ context }) => {
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
+    
     render({ time,playhead }) {
-      mesh.rotation.y = playhead*Math.PI*2;
-      material.uniforms.time.value = time;
+        
       // material.wireframe = true;
+      
+      
       controls.update();
       renderer.render(scene, camera);
     },
---
+++ shader5.js
@@ -0,0 +1,140 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+const Random = require("canvas-sketch-util/random");
+const canvasSketch = require("canvas-sketch");
+const glsl = require("glslify");
+const settings = {
+    duration:5,
+    dimensions:[1080,1080],
+    fps:60,
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl"
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas
+  });
+
+  // WebGL background color
+  renderer.setClearColor("#fff", 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(0, 0, -4);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  const geometry = new THREE.SphereGeometry(1, 32, 16);
+  const baseGem  = new THREE.IcosahedronGeometry(1, 1);
+  const circleGem = new THREE.CircleGeometry(1,32);
+  const points = baseGem.attributes.position.array;
+  console.log(circleGem);
+  
+  
+//   const geometry = new THREE.TorusGeometry(1,0.5,32, 16);
+  const vertexShader = /* glsl*/ `
+  varying vec2 vUv;
+  varying vec3 vPosition;
+    void main() {
+      vUv = uv;
+      vPosition= position;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
+    }
+  `;
+  const fragmentShader =glsl( /* glsl */`
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: aastep = require('glsl-aastep');
+  varying vec2 vUv;
+  uniform vec3 color;
+  uniform float time;
+  varying vec3 vPosition;
+  uniform vec3 points[POINT_COUNT];
+  uniform mat4 modelMatrix;
+
+    float sphereRim (vec3 spherePosition) {
+        vec3 normal = normalize(spherePosition.xyz);
+        vec3 worldNormal = normalize(mat3(modelMatrix) * normal.xyz);
+        vec3 worldPosition = (modelMatrix * vec4(spherePosition, 1.0)).xyz;
+        vec3 V = normalize(cameraPosition - worldPosition);
+        float rim = 1.0 - max(dot(V, worldNormal), 0.0);
+        return pow(smoothstep(0.0, 1.0, rim), 0.5);
+    }
+
+    void main() {
+        float dist = 1000.0;
+        for (int i = 0; i < POINT_COUNT; i++) {
+          vec3 point = points[i];
+          float curDist = distance(vPosition, point);
+          dist = min(curDist, dist);
+        }
+        float mask = aastep(0.15,dist);
+        mask = 1.0 - mask;
+        
+        vec3 fragColor = mix(color, vec3(1.0), mask);
+        float rim = sphereRim(vPosition);
+        fragColor += rim * 0.25;
+        gl_FragColor = vec4(vec3( fragColor),1.0);
+    }
+  `);
+
+  // Setup a material
+  const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    defines:{
+        POINT_COUNT:points.length
+    },
+    extensions:{
+        derivatives:true,
+    },
+    uniforms:{
+    points:{value:points},
+      time:{value:0},
+      color:{value:new THREE.Color('tomato')}
+    },
+    // wireframe: true
+  });
+ 
+  // Setup a mesh with geometry + material
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time,playhead }) {
+      mesh.rotation.y = playhead*Math.PI*2;
+      material.uniforms.time.value = time;
+      // material.wireframe = true;
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
